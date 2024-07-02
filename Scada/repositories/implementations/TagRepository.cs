43a8ff5389bd156280f9b15508ecf882ab4c03d3
maric300 <branko.maric300@gmail.com>
2024-07-02 12:21:16 +0200

    feat: added crud operations for tag repository

---
+++ Scada/repositories/implementations/TagRepository.cs
@@ -0,0 +1,50 @@
+using Scada.repositories.interfaces;
+using Scada.repositories;
+using System.Collections.Generic;
+using System.Linq;
+
+public class TagRepository : ITagRepository
+{
+    private readonly ScadaContext context;
+
+    public TagRepository(ScadaContext context)
+    {
+        this.context = context;
+    }
+
+    public void AddTag(Tag tag)
+    {
+        context.Tags.Add(tag);
+        context.SaveChanges();
+    }
+
+    public void RemoveTag(string tagId)
+    {
+        var tag = context.Tags.Find(tagId);
+        if (tag != null)
+        {
+            context.Tags.Remove(tag);
+            context.SaveChanges();
+        }
+    }
+
+    public void UpdateTag(Tag tag)
+    {
+        var existingTag = context.Tags.Find(tag.Id);
+        if (existingTag != null)
+        {
+            context.Entry(existingTag).CurrentValues.SetValues(tag);
+            context.SaveChanges();
+        }
+    }
+
+    public Tag GetTag(string tagId)
+    {
+        return context.Tags.Find(tagId);
+    }
+
+    public List<Tag> GetAllTags()
+    {
+        return context.Tags.ToList();
+    }
+}
---
+++ Scada/repositories/interfaces/ITagRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scada.repositories.interfaces
+{
+    public interface ITagRepository
+    {
+        void AddTag(Tag tag);
+        void RemoveTag(string tagId);
+        void UpdateTag(Tag tag);
+        Tag GetTag(string tagId);
+        List<Tag> GetAllTags();
+    }
+
+}
