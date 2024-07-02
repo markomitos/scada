a11114fb42d692f5f8744e60364dd5597d4e7b4a
maric300 <branko.maric300@gmail.com>
2024-07-02 22:19:27 +0200

    feat: added menu for adding new tags

--- DatabaseManagerApp/Program.cs
+++ DatabaseManagerApp/Program.cs
@@ -1,11 +1,16 @@
 ﻿using System;
+using System.Net;
+using System.Xml.Linq;
+using DatabaseManagerApp.TagServiceReference;
 using DatabaseManagerApp.UserServiceReference;
+using Scada.models;
 
 namespace DatabaseManagerApp
 {
     internal class Program
     {
         private static UserServiceReference.UserServiceClient userServiceClient = new UserServiceClient();
+        private static TagServiceReference.TagServiceClient tagServiceClient = new TagServiceClient();
         private static string currentToken = null;
 
         static void Main(string[] args)
@@ -58,7 +63,7 @@ namespace DatabaseManagerApp
         {
             Console.WriteLine("SCADA System - User Management (Logged In)");
             Console.WriteLine($"Current Token: {currentToken}");
-            Console.WriteLine("1. Some Action"); // Example new option for logged-in users
+            Console.WriteLine("1. Add tag"); // Example new option for logged-in users
             Console.WriteLine("2. Another Action"); // Example new option for logged-in users
             Console.WriteLine("3. Logout");
             Console.Write("Select an option: ");
@@ -68,7 +73,7 @@ namespace DatabaseManagerApp
             switch (choice)
             {
                 case "1":
-                    PerformSomeAction();
+                    AddTagMenu();
                     break;
                 case "2":
                     PerformAnotherAction();
@@ -80,6 +85,200 @@ namespace DatabaseManagerApp
                     Console.WriteLine("Invalid option. Please try again.");
                     break;
             }
+        }
+
+        private static void AddTagMenu()
+        {
+            Console.WriteLine("1. Add analog input tag");
+            Console.WriteLine("2. Add digital input tag");
+            Console.WriteLine("3. Add analog output tag");
+            Console.WriteLine("4. Add adigital output tag");
+            Console.WriteLine("5. Exit");
+            Console.Write("Select an option: ");
+
+            string choice = Console.ReadLine();
+            switch (choice)
+            {
+                case "1":
+                    AddAnalogInputTag();
+                    break;
+                case "2":
+                    AddDigitalInputTag();
+                    break;
+                case "3":
+                    AddAnalogOutputTag();
+                    break;
+                case "4":
+                    AddDigitalOutputTag();
+                    break;
+                case "5":
+                    return;
+                default:
+                    Console.WriteLine("Invalid option. Please try again.");
+                    break;
+            }
+        }
+
+        private static void AddAnalogInputTag()
+        {
+            string tagName = EnterTagName();
+
+            Console.Write("Enter tag description: ");
+            string tagDescription = Console.ReadLine();
+
+            Console.Write("Enter tag IO address: ");
+            string tagIoAddress = Console.ReadLine();
+
+            Console.Write("Enter tag driver [SIM/RT]: ");
+            string tagDriver = Console.ReadLine();
+
+            Console.Write("Enter tag scan time (ms): ");
+            string tagScanTime = Console.ReadLine();
+
+            Console.Write("Enter tag on/off scan [true/false]: ");
+            string tagOnOffScan = Console.ReadLine();
+
+            Console.Write("Enter tag low limit: ");
+            string tagLowLimit = Console.ReadLine();
+
+            Console.Write("Enter tag high limit: ");
+            string tagHighLimit = Console.ReadLine();
+
+            Console.Write("Enter tag units: ");
+            string tagUnits = Console.ReadLine();
+
+            Console.Write("Enter tag alarms [true/false]: ");
+            string tagAlarms = Console.ReadLine();
+
+            AnalogInputTag tag = new AnalogInputTag
+            {
+                Name = tagName,
+                Description = tagDescription,
+                IoAddress = tagIoAddress,
+                Driver = tagDriver,
+                ScanTime = int.Parse(tagScanTime),
+                OnOffScan = bool.Parse(tagOnOffScan),
+                LowLimit = double.Parse(tagLowLimit),
+                HighLimit = double.Parse(tagHighLimit),
+                Units = tagUnits,
+                Alarms = bool.Parse(tagAlarms)
+            };
+
+               tagServiceClient.AddTag(tag);
+        }
+
+        private static void AddDigitalInputTag()
+        {
+            string tagName = EnterTagName();
+
+            Console.Write("Enter tag description: ");
+            string tagDescription = Console.ReadLine();
+
+            Console.Write("Enter tag driver [SIM/RT]: ");
+            string tagDriver = Console.ReadLine();
+
+            Console.Write("Enter tag IO address [R/C/S/IP_Address]: ");
+            string tagIoAddress = Console.ReadLine();
+
+            Console.Write("Enter tag scan time (ms): ");
+            string tagScanTime = Console.ReadLine();
+
+            Console.Write("Enter tag on/off scan [true/false]: ");
+            string tagOnOffScan = Console.ReadLine();
+
+            Tag tag = new DigitalInputTag
+            {
+                Name = tagName,
+                Description = tagDescription,
+                IoAddress = tagIoAddress,
+                Driver = tagDriver,
+                ScanTime = int.Parse(tagScanTime),
+                OnOffScan = bool.Parse(tagOnOffScan),
+            };
+
+            tagServiceClient.AddTag(tag);
+        }
+
+        private static void AddAnalogOutputTag()
+        {
+            string tagName = EnterTagName();
+
+            Console.Write("Enter tag description: ");
+            string tagDescription = Console.ReadLine();
+
+            Console.Write("Enter tag IO address [R/C/S/IP_Address]: ");
+            string tagIoAddress = Console.ReadLine();
+
+            Console.Write("Enter tag initial value: ");
+            string tagInitialValue = Console.ReadLine();
+
+            Console.Write("Enter tag low limit: ");
+            string tagLowLimit = Console.ReadLine();
+
+            Console.Write("Enter tag high limit: ");
+            string tagHighLimit = Console.ReadLine();
+
+            Console.Write("Enter tag units: ");
+            string tagUnits = Console.ReadLine();
+
+            Tag tag = new AnalogOutputTag
+            {
+                Name = tagName,
+                Description = tagDescription,
+                IoAddress = tagIoAddress,
+                LowLimit = double.Parse(tagLowLimit),
+                HighLimit = double.Parse(tagHighLimit),
+                Units = tagUnits,
+                InitialValue = double.Parse(tagInitialValue)
+            };
+
+            tagServiceClient.AddTag(tag);
+        }
+
+        private static void AddDigitalOutputTag()
+        {
+            string tagName = EnterTagName();
+
+            Console.Write("Enter tag description: ");
+            string tagDescription = Console.ReadLine();
+
+            Console.Write("Enter tag IO address [R/C/S/IP_Address]: ");
+            string tagIoAddress = Console.ReadLine();
+
+            Console.Write("Enter tag initial value: ");
+            string tagInitialValue = Console.ReadLine();
+
+            Tag tag = new DigitalOutputTag
+            {
+                Name = tagName,
+                Description = tagDescription,
+                IoAddress = tagIoAddress,
+                InitialValue = double.Parse(tagInitialValue)
+            };
+
+            tagServiceClient.AddTag(tag);
+        }
+
+        private static string EnterTagName()
+        {
+            string tagName;
+            bool isUnique;
+
+            do
+            {
+                Console.Write("Enter tag name: ");
+                tagName = Console.ReadLine();
+
+                isUnique = tagServiceClient.IsTagNameUnique(tagName);
+
+                if (!isUnique)
+                {
+                    Console.WriteLine("Tag name already exists. Please enter a unique tag name.");
+                }
+
+            } while (!isUnique);
+
+            return tagName;
         }
 
         private static void RegisterUser()
