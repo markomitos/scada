0d1c070a16460a1fac3c3bcc2f391a23a4ac2ed6
OgnjenGligoric <gligoric383@gmail.com>
2024-07-08 11:47:12 +0200

    alarm repository

--- Scada/models/Alarm.cs
+++ Scada/models/Alarm.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+using System.Xml.Serialization;
 
 namespace Scada.models
 {
@@ -9,12 +11,29 @@ namespace Scada.models
     {
         LOW, HIGH
     }
+    [XmlRoot("alarm")]
     public class Alarm
     {
+        [Key]
+        public int Id { get; set; }
+        [XmlAttribute("Name")]
+        [Required]
+        public string Name{ get; set; }
+        [XmlAttribute("Type")]
+        [Required]
         public AlarmType Type {  get; set; }
+        [XmlAttribute("Priority")]
+        [Required]
         public int Priority {  get; set; }
+        [XmlAttribute("Treshold")]
+        [Required]
         public Double Treshold { get; set; }
+        [XmlAttribute("Unit")]
+        [Required]
         public string Unit { get; set; }
+        [XmlAttribute("TagName")]
+        [Required] 
+        public int TagName { get; set; }
 
     }
 }
---
+++ Scada/repositories/implementations/AlarmRepository.cs
@@ -0,0 +1,76 @@
+﻿using Scada.models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Xml.Serialization;
+
+namespace Scada.repositories.implementations
+{
+    public class AlarmRepository
+    {
+        private List<Alarm> alarms = new List<Alarm>();
+        private string file = HttpContext.Current.Server.MapPath("~/Resources/alarmConfig.xml");
+
+        public AlarmRepository() 
+        {
+            LoadAlarms();
+        }
+
+        private void LoadAlarms()
+        {
+            if (File.Exists(file))
+            {
+                try
+                {
+                    XmlSerializer xmlSerializer = new XmlSerializer(typeof(List<Alarm>), new XmlRootAttribute("Alarms"));
+                    using (FileStream fs = new FileStream(file, FileMode.Open))
+                    {
+                        alarms = (List<Alarm>)xmlSerializer.Deserialize(fs);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error while loading: " + ex.Message);
+                }
+            }
+        }
+
+        public void AddAlarm(Alarm alarm)
+        {
+            alarms.Add(alarm);
+            Save();
+        }
+
+        public bool RemoveTag(string name)
+        {
+            Alarm alarmToRemove = alarms.FirstOrDefault(alarm => alarm.Name == name);
+
+            if (alarmToRemove != null)
+            {
+                alarms.Remove(alarmToRemove);
+                Save();
+                return true;
+            }
+
+            return false;
+        }
+
+        private void Save()
+        {
+            try
+            {
+                XmlSerializer xmlSerializer = new XmlSerializer(typeof(List<Alarm>), new XmlRootAttribute("Alarms"));
+                using (TextWriter writer = new StreamWriter(file))
+                {
+                    xmlSerializer.Serialize(writer, alarms);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error while saving: " + ex.Message);
+            }
+        }
+    }
+}
